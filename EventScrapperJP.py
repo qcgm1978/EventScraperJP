2ed36000f32082e0eac6c0f74ed6a815378c4e0b
Masterfor07 <f.kozlowski0707@gmail.com>
2025-01-15 20:43:36 +0100

    Added Place

--- EventScrapperJP.py
+++ EventScrapperJP.py
@@ -1,32 +1,20 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 import openpyxl
 import pykakasi
 import pandas as pd
 
-url = "https://t.pia.jp/music/"
-pagePiaC = requests.get(url)
-pagePiaC.encoding = 'utf-8'
-html_content_PiaC = pagePiaC.text
+def doc_Pia_from_url(url):
+    pagePia = requests.get(url)
+    pagePia.encoding = 'utf-8'
+    html_content_Pia = pagePia.text
+    doc_Pia = BeautifulSoup(html_content_Pia, "html.parser")
+    return doc_Pia
 
-
-doc_PiaC = BeautifulSoup(html_content_PiaC, "html.parser")
-
-url2 = "https://t.pia.jp/anime/"
-pagePiaA = requests.get(url2)
-pagePiaA.encoding = 'utf-8'
-html_content_PiaA = pagePiaA.text
-
-
-doc_PiaA = BeautifulSoup(html_content_PiaA, "html.parser")
-
-url3 = "https://t.pia.jp/event/"
-pagePiaE = requests.get(url3)
-pagePiaE.encoding = 'utf-8'
-html_content_PiaE = pagePiaE.text
-
-
-doc_PiaE = BeautifulSoup(html_content_PiaA, "html.parser")
+doc_PiaM = doc_Pia_from_url("https://t.pia.jp/music/")
+doc_PiaA = doc_Pia_from_url("https://t.pia.jp/anime/")
+doc_PiaE = doc_Pia_from_url("https://t.pia.jp/event/")
 
 def convert_to_romaji(japanese_text):
     kks = pykakasi.kakasi()
@@ -34,7 +22,14 @@ def convert_to_romaji(japanese_text):
     romaji_text = ''.join([item['hepburn'] for item in result])
     return romaji_text
 
+def PiaInnerScrapper(url):
+    Inner_doc_Pia = doc_Pia_from_url(url)
+    PPia = Inner_doc_Pia.find(string=re.compile("会場.*"))
+    return convert_to_romaji(PPia)
+     
+
 def PiaScrapper(doc_Pia):
+    i=0
     concerts = []
     for div_Pia in doc_Pia.find_all("div"):
         
@@ -45,34 +40,38 @@ def PiaScrapper(doc_Pia):
             
             if figcaption_Pia:
                 
-                name_concert_Pia = figcaption_Pia.find("h2")
-                namePia = name_concert_Pia.get_text(strip=True) if name_concert_Pia else None
+                name_Pia = figcaption_Pia.find("h2")
+                namePia = name_Pia.get_text(strip=True) if name_Pia else None
                 
                 if namePia:
-                    RomajiPia = convert_to_romaji(namePia)
+                    romajiPia = convert_to_romaji(namePia)
                 else:
-                    RomajiPia = None
+                    romajiPia = None
                     
-                date_concert_Pia= figcaption_Pia.find("span")
-                datePia = date_concert_Pia.get_text(strip=True) if date_concert_Pia else None
+                date_Pia= figcaption_Pia.find("span")
+                datePia = date_Pia.get_text(strip=True) if date_Pia else None
                 
                 linkPia = a_tag_Pia.get("href") if a_tag_Pia else None
-                
+                               
+                placePia = PiaInnerScrapper(linkPia)
+                                 
                 if namePia and datePia and linkPia:
                     if not any(linkPia in concert["Link"] for concert in concerts):
-                        concerts.append({"Name": namePia, "Romaji": RomajiPia, "Date": datePia, "Link": linkPia})
+                        concerts.append({"Name": namePia, "Romaji": romajiPia, "Place": placePia, "Date": datePia, "Link": linkPia})
+                        i+=1
+                        print(i)                       
     return concerts
 
-concerts = PiaScrapper(doc_PiaC) + PiaScrapper(doc_PiaA) + PiaScrapper(doc_PiaE)
+concerts = PiaScrapper(doc_PiaM) + PiaScrapper(doc_PiaA) + PiaScrapper(doc_PiaE)
 
 excel_file = "Events.xlsx"
 workbook = openpyxl.Workbook()
 sheet = workbook.active
 sheet.title = "EventsJP2025"
-sheet.append(["Name", "Romaji", "Date", "Link"])
+sheet.append(["Name", "Romaji", "Place", "Date", "Link"]) #If new column added, change.
 
 for concert in concerts:
-    sheet.append([concert["Name"], concert["Romaji"], concert["Date"], concert["Link"]])
+    sheet.append([concert["Name"], concert["Romaji"], concert["Place"], concert["Date"], concert["Link"]])
 
 workbook.save(excel_file)
 
@@ -85,7 +84,7 @@ def remove_duplicates_in_excel(file_name):
 
     seen = set()
     for row in rows[1:]:
-        link = row[3] #Change if columns moved
+        link = row[4] #Change if columns moved
         if link not in seen:
             unique_rows.append(row)
             seen.add(link)
@@ -95,7 +94,6 @@ def remove_duplicates_in_excel(file_name):
         sheet.append(unique_row)
 
     workbook.save(file_name)
-
 remove_duplicates_in_excel(excel_file)
 
 print(f"Scraped {len(concerts)} concerts. Data saved to {excel_file}.")
