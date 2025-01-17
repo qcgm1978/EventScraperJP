3bcb9e64e2e57d89e9aa963c46a3b0fc6ae51694
Masterfor07 <f.kozlowski0707@gmail.com>
2025-01-17 23:03:46 +0100

    Split the file for clarity. Added the helper file with functions in general use. Reverted the dupliactes pia function

--- EventScrapperJP.py
+++ EventScrapperJP.py
@@ -9,30 +9,11 @@ import os
 from openpyxl.styles import Font, Color, PatternFill, Alignment, Fill
 from openpyxl.worksheet.dimensions import ColumnDimension, DimensionHolder
 from openpyxl.utils import get_column_letter
+import helper
 EXCEL_FILE = "EventsJP2025.xlsx"
 
-def save_workbook(workbook):    
-    try:
-        workbook.save(EXCEL_FILE)
-    except: 
-        print("Close Excel you imbecile!")
-        time.sleep(15)
-        save_workbook(workbook)
-
-def convert_to_romaji(japanese_text):
-    kks = pykakasi.kakasi()
-    result = kks.convert(japanese_text)
-    romaji_text = ''.join([item['hepburn'] for item in result])
-    return romaji_text
-
-def doc_from_url(url):
-    pagePia = requests.get(url)
-    pagePia.encoding = 'utf-8'
-    html_content_Pia = pagePia.text
-    return BeautifulSoup(html_content_Pia, "html.parser")
-
 def PiaInnerScrapper(url):
-    Inner_doc_Pia = doc_from_url(url)
+    Inner_doc_Pia = helper.doc_from_url(url)
     for div_Inner_Pia in Inner_doc_Pia.find_all("div", class_="textDefinitionList-2024__item"):
         dt_tag_Pia = div_Inner_Pia.find("dt", class_="textDefinitionList-2024__title")
         if dt_tag_Pia and "会場" in dt_tag_Pia.get_text(strip=True):
@@ -59,7 +40,7 @@ def PiaScrapper(doc_Pia):
                 namePia = name_Pia.get_text(strip=True) if name_Pia else None
                 
                 if namePia:
-                    romajiPia = convert_to_romaji(namePia)
+                    romajiPia = helper.convert_to_romaji(namePia)
                 else:
                     romajiPia = None
                     
@@ -91,7 +72,7 @@ def eplusScrapper(doc_eplus, month): #fix the dates edge case
         for page in range(1, max_pages+1):
             url = f"https://eplus.jp/sf/event/month-0{month}/p{page}"
             print(f"Scraping page: {url}")
-            doc_eplus = doc_from_url(url)
+            doc_eplus = helper.doc_from_url(url)
             
             ticket_div = doc_eplus.find("div", class_="block-ticket-list__content output")
             tickets = ticket_div.find_all("a")
@@ -100,7 +81,7 @@ def eplusScrapper(doc_eplus, month): #fix the dates edge case
                 nameEplus = (ticket.find("h3", class_="ticket-item__title")).get_text(strip=True)
                 
                 if nameEplus:
-                    romajiEplus = convert_to_romaji(nameEplus)
+                    romajiEplus = helper.convert_to_romaji(nameEplus)
                 else:
                     romajiEplus = None
                 
@@ -123,37 +104,25 @@ def eplusScrapper(doc_eplus, month): #fix the dates edge case
     print(f"Finished scraping current site. Proceeding to the next one.")
     return Eplusconcerts
 
-def OpenSheet(sheet_name, header):
-    if os.path.exists(EXCEL_FILE):
-        workbook = openpyxl.load_workbook(EXCEL_FILE)
-    else:
-        workbook = openpyxl.Workbook()
-        workbook.remove(workbook.active)
-    if sheet_name in workbook.sheetnames:
-        sheet = workbook[sheet_name]
-    else:
-        sheet = workbook.create_sheet(title=sheet_name)
-        sheet.append(header) 
-    sheet = workbook[sheet_name]
-    return workbook, sheet
-
 def remove_duplicates_in_excel_pia():
     workbook = openpyxl.load_workbook(EXCEL_FILE)
     sheet = workbook["Events_t.pia.jp"]
-    
+    rows = list(sheet.iter_rows(values_only=True))
+    headers = rows[0]
+    unique_rows = [headers]
+
     seen = set()
-    
-    row_number = 2
-    while row_number <= sheet.max_row:
-        link = sheet.cell(row=row_number, column=5).value  #Link column (adjust if moved)
-        
-        if link in seen:
-            sheet.delete_rows(row_number)
-        else:
+    for row in rows[1:]:
+        link = row[4] #Change if columns moved
+        if link not in seen:
+            unique_rows.append(row)
             seen.add(link)
-            row_number += 1
-    
-    save_workbook(workbook)
+
+    sheet.delete_rows(1, sheet.max_row)
+    for unique_row in unique_rows:
+        sheet.append(unique_row)
+
+    helper.save_workbook(workbook)
     
 def remove_duplicates_in_excel_eplus():
     workbook = openpyxl.load_workbook(EXCEL_FILE)
@@ -176,76 +145,43 @@ def remove_duplicates_in_excel_eplus():
             seen[name] = row_number
             row_number += 1
     
-    save_workbook(workbook)
+    helper.save_workbook(workbook)
     
-def style_sort_excel(sheet_name, sorting_column):
-    workbook = openpyxl.load_workbook(EXCEL_FILE)
-    sheet = workbook[sheet_name]
-    row_count = sheet.max_row
-    column_count = sheet.max_column
-    
-    df = pd.DataFrame(sheet.values)
-    headers = df.iloc[0]
-    df = df[1:]
-    df.columns = headers
-    df = df.sort_values(by=sorting_column, ascending=True)
-    sheet.delete_rows(1, sheet.max_row)
-    for row in [df.columns.tolist()] + df.values.tolist():
-       sheet.append(row)
-    
-    title_row_style = Font(size=14, color="FFFFFF", bold=True)
-    for i in range (0,column_count):
-        sheet.cell(row=1, column=i+1).font = title_row_style
-    dim_holder = DimensionHolder(worksheet=sheet)
-    for col in range(sheet.min_column, sheet.max_column + 1):
-        dim_holder[get_column_letter(col)] = ColumnDimension(sheet, min=col, max=col, width=35)
-    sheet.column_dimensions = dim_holder
-    
-    for z in range (0, column_count):
-        sheet.cell(row=1, column = z + 1).fill = PatternFill(start_color="38AA49", end_color="38AA49", fill_type="solid")
-    for x in range(2, row_count):
-        for z in range (0, column_count): 
-            c = sheet.cell(row=x, column=z + 1)
-            if x % 2 != 0:
-                c.fill = PatternFill(start_color="ACFFB8", end_color="ACFFB8", fill_type="solid")
-    if row_count % 2 != 0:
-        for z in range (0, column_count):
-            l = sheet.cell(row=row_count, column=z + 1)
-            l.fill = PatternFill(start_color="ACFFB8", end_color="ACFFB8", fill_type="solid")
-    
-    save_workbook(workbook)
 
-doc_PiaM = doc_from_url("https://t.pia.jp/music/")
-doc_PiaA = doc_from_url("https://t.pia.jp/anime/")
-doc_PiaE = doc_from_url("https://t.pia.jp/event/")
+    
+    helper.save_workbook(workbook)
+
+doc_PiaM = helper.doc_from_url("https://t.pia.jp/music/")
+doc_PiaA = helper.doc_from_url("https://t.pia.jp/anime/")
+doc_PiaE = helper.doc_from_url("https://t.pia.jp/event/")
 
 Piaconcerts = PiaScrapper(doc_PiaM) + PiaScrapper(doc_PiaA) + PiaScrapper(doc_PiaE)
 
 sheet_name = "Events_t.pia.jp"
 header = ["Name", "Romaji", "Place", "Date", "Link"] #If new column added, change.
 
-workbook, sheet = OpenSheet(sheet_name, header)    
+workbook, sheet = helper.OpenSheet(sheet_name, header)    
     
 for Piaconcert in Piaconcerts:
     sheet.append([Piaconcert["Name"], Piaconcert["Romaji"], Piaconcert["Place"], Piaconcert["Date"], Piaconcert["Link"]])
     
-save_workbook(workbook)
+helper.save_workbook(workbook)
 
 remove_duplicates_in_excel_pia()
 
-style_sort_excel(sheet_name, "Date")
+helper.style_sort_excel(sheet_name, "Date")
 
 print(f"Done! Scraped t.pia.jp. Data saved to {EXCEL_FILE}.")
 
 ## Here we start scrapping eplus.jp ##
 
 try:
-    doc_eplus_april = doc_from_url("https://eplus.jp/sf/event/month-04")
+    doc_eplus_april = helper.doc_from_url("https://eplus.jp/sf/event/month-04")
 except:
     print("Error with eplus.jp. It's probably asleep. Trying again later.")
     exit()
 try:
-    doc_eplus_may = doc_from_url("https://eplus.jp/sf/event/month-05")
+    doc_eplus_may = helper.doc_from_url("https://eplus.jp/sf/event/month-05")
 except:
     print("Error with eplus.jp. It's probably asleep. Trying again later.")
     exit()
@@ -255,14 +191,14 @@ Eplusconcerts = eplusScrapper(doc_eplus_april, 4) + eplusScrapper(doc_eplus_may,
 sheet_name = "Events_eplus.jp"
 header = ["Name", "Romaji", "Place", "Beginning Date", "Ending Date", "Link"] #If new column added, change.
 
-workbook, sheet = OpenSheet(sheet_name, header)
+workbook, sheet = helper.OpenSheet(sheet_name, header)
 
 for Eplusconcert in Eplusconcerts:
     sheet.append([Eplusconcert["Name"], Eplusconcert["Romaji"], Eplusconcert["Place"], Eplusconcert["Date"], Eplusconcert["Date"], Eplusconcert["Link"]])
-save_workbook(workbook)
+helper.save_workbook(workbook)
 
 remove_duplicates_in_excel_eplus()
 
-style_sort_excel(sheet_name, "Beginning Date") #maybe a problem with beginning date?
+helper.style_sort_excel(sheet_name, "Beginning Date") #maybe a problem with beginning date?
 
 print(f"Done! Scraped eplus.jp. Data saved to {EXCEL_FILE}.")
