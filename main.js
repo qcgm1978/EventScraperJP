92ef14b0aefe79b8644da0681a02f5611f2968e9
Masterfor07 <f.kozlowski0707@gmail.com>
2025-01-31 23:25:46 +0100

    Updated package json and added code for main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
+
+let mainWindow;
+let pythonProcess;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'renderer.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', () => {
+  pythonProcess = spawn('python', ['python-backend/app.py']);
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on('quit', function () {
+  pythonProcess.kill();
+});
